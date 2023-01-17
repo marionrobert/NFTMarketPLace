6c8da4e36f677144f73ea15eb82a78bc8fc874d3
Marion Robert <mrobert148@gmail.com>
2023-01-17 19:19:05 +0100

    render all NFTs of one particular owner in front using Gallery/header & item component

--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import homeImage from "../../assets/home-img.png";
 import Minter from "./Minter";
 import Gallery from "./Gallery";
+import { opend } from "../../../declarations/opend";
+import CURRENT_USER_ID from "../index";
 
 function Header() {
+
+  const [userOwnedGallery, setOwnedGallery] = useState();
+
+  async function getNFTs() {
+    const userNFTIds = await opend.getOwnedNFTs(CURRENT_USER_ID);
+    console.log(userNFTIds);
+    setOwnedGallery(<Gallery title="My NFTS" ids={userNFTIds}/>)
+  };
+
+  useEffect(() => {
+    getNFTs();
+  }, []);
+
   return (
-    <BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
       <div className="app-root-1">
         <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
           <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
@@ -42,7 +57,7 @@ function Header() {
           <Minter/>
         </Route>
         <Route exact path="/collection">
-          <Gallery title="My NFTS"/>
+          {userOwnedGallery}
         </Route>
       </Switch>
     </BrowserRouter>
